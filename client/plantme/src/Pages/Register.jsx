f8efcf357e0952f654997b6bcf523c51d6b1362d
Md Eftequarul Alam <efthaqur@gmail.com>
2025-12-02 01:02:52 +0600

    Add: Register form

--- client/plantme/src/Pages/Register.jsx
+++ client/plantme/src/Pages/Register.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 const Register = () => {
+  
+  const handleRegister =() => {
+    console.log('Register Clicked!');
+    
+  }
   return (
     <>
-      <div>
+      <div className="container mx-auto w-4xl justify-center items-center m-10">
         <p>Hello Visitor! Please Register!!</p>
+        <form>
+          <input type="text" name="name"  />
+          <input type="email" name="email" />
+          <input type="password" name="password" />
+          <button onSubmit={handleRegister()} className="btn btn-primary">Register</button>
+        </form>
       </div>
     </>
   );
